5834c12e62df079c2fef9fff9e52234b40082731
MartinFlakes <martin.rayer19@gmail.com>
2025-01-25 22:27:54 -0600

    Se inicializa laravel, boostrap y vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import MyComponent from './components/MyComponent.vue';
+
+const app = createApp({});
+app.component('my-component', MyComponent);
+
+app.mount('#app');
