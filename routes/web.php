9e2a265dc852a318ecca0e1631b7542b4427e848
MartinFlakes <martin.rayer19@gmail.com>
2025-01-26 23:02:14 -0600

    Se agrega ruta welcome

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 
+Route::get('/', function () { return view('welcome'); });
+
+//Vistas de productos
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
-
-Route::get('/', function () {
-    return view('welcome');
-});
